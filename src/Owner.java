4adacd87b6d234a3c462cb958c90c704c2d2e4b1
afik <k.afis3@rocketmail.com>
2014-05-10 14:41:04 +0700

    edit owner

--- src/Owner.java
+++ src/Owner.java
@@ -1,9 +1,57 @@
-import 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Afik
+ */
+
+import java.util.*;
+import javax.swing.*;
 
 public class Owner {
-	public :
+    private int sisaWaktu;
+    private Queue posisi = new LinkedList();
+    private Deque activities = new LinkedList();
+    
+    public void setSisaWaktu(int sisaWaktu) {
+        this.sisaWaktu = sisaWaktu;
+    }
 
-	private :
-		int waktuSisa;
-		queue
+    public void setPosisi(Queue posisi) {
+        this.posisi = posisi;
+    }
+
+    public void setActivities(Deque activities) {
+        this.activities = activities;
+    }
+
+    public int getSisaWaktu() {
+        return sisaWaktu;
+    }
+
+    public Queue getPosisi() {
+        return posisi;
+    }
+
+    public Deque getActivities() {
+        return activities;
+    }
+    
+    public Queue CariPath(int[] tujuan) {
+        Queue Q = new LinkedList();
+        
+        return Q;
+    }
+    
+    public void Draw(){
+        
+    }
+    
+    public void Update(long elapsedTime){
+        
+    }
 }

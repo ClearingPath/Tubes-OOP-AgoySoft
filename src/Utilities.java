86d3e54ad9541e346efb79fbb728edf86966f246
afik <k.afis3@rocketmail.com>
2014-05-15 13:47:53 +0700

    add utilities

---
+++ src/Utilities.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Afik
+ */
+public class Utilities {
+    public enum TileType {
+       Normal, KamarMandi, 
+    }
+    
+    public enum ItemType {
+        Soap, Food, Chair, Lemari, Bed, Table;
+    }
+    
+}

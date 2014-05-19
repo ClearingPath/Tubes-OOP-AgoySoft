2732ef26dba817a75dbca5e7d014ebd9c2a701e1
rakhmatullahyoga <13512053@std.stei.itb.ac.id>
2014-05-19 13:51:18 +0700

    v 0.99

--- src/Utilities.java
+++ src/Utilities.java
@@ -11,8 +11,8 @@
 
 public class Utilities {
     //ukuran asli peta
-    public static int MAP_ROW_COUNT=21;
-    public static int MAP_COL_COUNT=21;
+    public static int MAP_ROW_COUNT=24;
+    public static int MAP_COL_COUNT=24;
 
     //ukuran peta yang dikasih lihat
     public static final int VIEW_ROW_COUNT=12;

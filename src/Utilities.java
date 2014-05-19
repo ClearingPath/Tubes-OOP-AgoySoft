fabd7c1c07f7dae4d3d8ee927d8347f9ac244f9c
afik <k.afis3@rocketmail.com>
2014-05-19 17:25:57 +0700

    edit img

--- src/Utilities.java
+++ src/Utilities.java
@@ -11,8 +11,8 @@
 
 public class Utilities {
     //ukuran asli peta
-    public static int MAP_ROW_COUNT=24;
-    public static int MAP_COL_COUNT=24;
+    public static int MAP_ROW_COUNT;
+    public static int MAP_COL_COUNT;
 
     //ukuran peta yang dikasih lihat
     public static final int VIEW_ROW_COUNT=12;
